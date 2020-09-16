3dbe4e4d500a49a2903e1a267f9484c5b746446b
Al-amin Sahed <alaminsahed97@gmail.com>
2020-09-16 12:40:03 +0600

    post data from website and read post api response

--- index.js
+++ index.js
@@ -26,7 +26,11 @@ app.get('/user/:id',(req,res)=>{   //:id determines the dynamic value according 
 //post [in large file page are different]
 
 app.post('/addUser',(req,res)=>{
-    console.log(req.body);
+    // console.log(req.body);
+    //save to database. database add id auto
+    const user = req.body;
+    user.id = 55; // add extra element in req.body
+    res.send(user);
 })
 
 app.listen(3000, ()=>console.log('Listenig to port 3000'));
